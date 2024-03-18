2557ebb1bf43c893c7627d45d7e537e0bb1d3549
Syed Saleem Raza Shahit <syedsaleemrazashah69@gmail.com>
2024-03-18 18:28:54 +0500

    Calculator

---
+++ app.js
@@ -0,0 +1,34 @@
+// Welcome Prompt On Web Page
+let username = prompt("What is Your Name");
+alert("Welcome "+ username);
+console.log(username);
+
+// User's Added Numbers Are Shown Here.
+let num1 = prompt("Enter Any First Number");
+let num2 = prompt("Enter Any Second Number");
+
+// Strings Convert into Numbers Here.
+num1 = Number(num1);
+num2 = Number(num2);
+console.log("num1: ", num1);
+console.log("num2: ", num2);
+
+// User's Numbers Are Addition & Prompted Here.
+let sum = num1 + num2;
+alert("Addition " + sum);
+console.log("Addition " + sum);
+
+// User's Numbers Are Subtraction & Prompted Here.
+let minus = num1 - num2;
+alert("Subtraction " + minus);
+console.log("Subtraction " + minus);
+
+// User's Numbers Are Multiplication & Prompted Here.
+let multiply = num1 * num2;
+alert("Multiplication " + num1 * num2);
+console.log("Multiplication " + num1 * num2);
+
+// User's Numbers Are Division & Prompted Here.
+let divide = num1 / num2;
+alert("Division " + num1 / num2);
+console.log("Division " + num1 / num2)
